abeaa6feacf4ce00948527432c8b42249acc9ca4
b0nz <elliantosarono.p@gmail.com>
2025-12-23 02:15:28 +0000

    chore: add garbage collector

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -33,4 +33,12 @@ in
   ];
 
   security.sudo.wheelNeedsPassword = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  nix.settings.auto-optimise-store = true;
 }
