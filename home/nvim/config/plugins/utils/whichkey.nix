e5160cba7a5a799e8c921485846d314e314779fc
b0nz <elliantosarono.p@gmail.com>
2025-12-29 04:28:36 +0000

    chore: update nvim configuration

--- home/nvim/config/plugins/utils/whichkey.nix
+++ home/nvim/config/plugins/utils/whichkey.nix
@@ -1,5 +1,86 @@
+{ pkgs, ... }:
 {
   plugins.which-key = {
     enable = true;
+    settings = {
+      preset = "modern";
+      delay = 200;
+      spec = [
+        {
+          __unkeyed-1 = "<leader><tab>";
+          group = "tabs";
+          icon = "󰓩 ";
+        }
+        {
+          __unkeyed-1 = "<leader>b";
+          group = "buffer";
+          icon = " ";
+        }
+        {
+          __unkeyed-1 = "<leader>c";
+          group = "code";
+          icon = " ";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "file/find";
+          icon = "󰈞 ";
+        }
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "git";
+          icon = "󰊢 ";
+        }
+        {
+          __unkeyed-1 = "<leader>q";
+          group = "quit/session";
+          icon = " ";
+        }
+        {
+          __unkeyed-1 = "<leader>u";
+          group = "ui";
+          icon = " ";
+        }
+        {
+          __unkeyed-1 = "<leader>w";
+          group = "windows";
+          icon = "󰖲 ";
+        }
+        {
+          __unkeyed-1 = "<leader>x";
+          group = "diagnostics/quickfix";
+          icon = "󱖫 ";
+        }
+      ];
+      icons = {
+        breadcrumb = "»";
+        separator = "󰁔";
+        group = "+";
+      };
+      win = {
+        border = "rounded";
+        no_overlap = false;
+        padding = [
+          1
+          2
+        ];
+        title = true;
+        title_pos = "center";
+        zindex = 1000;
+      };
+      # show_keys = true;
+      # show_help = true;
+      # triggers = "auto";
+      # triggers_blacklist = [
+      #   {
+      #     __unkeyed-1 = "<leader>!";
+      #     mode = "n";
+      #   }
+      #   {
+      #     __unkeyed-1 = "<leader>w";
+      #     mode = "n";
+      #   }
+      # ];
+    };
   };
 }
