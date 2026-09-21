5e87815710e383553566c19bfbfcbe69054971ae
b0nz <elliantosarono.p@gmail.com>
2026-09-21 13:11:50 +0700

    chore: update outputs.nix

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -56,6 +56,7 @@ flake-parts.lib.mkFlake { inherit inputs; } {
               useUserPackages = true;
               users.${user} = import ../home;
               extraSpecialArgs = specialArgs;
+              backupFileExtension = "backup";
             };
           }
         ];
